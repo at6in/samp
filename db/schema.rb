5f65660fdbdca6ad58a13418a9a52c06e1b4b25c
Atbin Jahandideh <atbinjahandideh@gmail.com>
2017-11-13 21:19:56 +0000

    add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112171813) do
+ActiveRecord::Schema.define(version: 20171113194528) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
+
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'stringreset_sent_at' for column 'reset_digest'
 
 end
